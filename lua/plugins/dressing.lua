8edce9bed1b42b323346eccd69c9ec886ef61cf6
Mathias Santos de Brito <mathias.brito@me.com>
2025-02-26 12:08:11 +0000

    pluings cleaned up and add snacks

--- lua/plugins/dressing.lua
+++
@@ -1,7 +0,0 @@
-return {
-	-- dressing
-	"stevearc/dressing.nvim",
-	config = function()
-		require("dressing").setup({})
-	end,
-}
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,11 +1,5 @@
 return {
 	"williamboman/mason.nvim",
-	dependencies = {
-		"nvim-telescope/telescope.nvim",
-		"williamboman/mason-lspconfig.nvim",
-	},
-	config = function()
-		local mason = require("mason")
-		mason.setup()
-	end,
+  "williamboman/mason-lspconfig",
+  "williamboman/nvim-lspconfig",
 }
